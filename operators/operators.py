3fbf9efa4ba4c276b70e4f61af324f16b108283b
tonyedgal <tonyedgal@gmail.com>
2023-08-26 20:28:13 +0100

    feat: arithmetic operators in python

---
+++ operators/operators.py
@@ -0,0 +1,32 @@
+name = 'Tony'  # assignment operator
+
+# Arithmetic operators
+
+2 + 3  # addition operator
+
+2 - 3  # subtraction operator
+
+2 * 3  # multiplication operator
+
+2 / 3  # division operator
+
+2 // 3  # floor division operator, rounds down to nearest whole number
+
+round(2 / 3)  # rounds up to nearest whole number
+
+24 % 3  # modulus operator, returns remainder
+
+2 ** 3  # exponent operator, 2 to the power of 3
+
+meaning = 42
+meaning += 10  # meaning = meaning + 10
+
+meaning -= 10  # meaning = meaning - 10
+
+meaning *= 10  # meaning = meaning * 10
+
+meaning /= 10  # meaning = meaning / 10, adds decimal point
+
+names = "Tony " + "Stark"  # concatenation operator
+
+print(names)
