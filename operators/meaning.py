af97b32e6b4c9deec799c2d4514d7de196f4b8bd
tonyedgal <tonyedgal@gmail.com>
2023-08-27 16:39:03 +0100

    feat: ternary operator in python

--- operators/meaning.py
+++ operators/meaning.py
@@ -1,8 +1,12 @@
 meaning = 8
 print('')
 
-if meaning > 10:
-    print('Correct')
-else:
-    print('Incorrect')
+# if meaning > 10:
+#     print('Correct')
+# else:
+#     print('Incorrect')
+
+# Terenary operator
+
+print('Correct') if meaning > 10 else print('Incorrect')
 print('')
