d238866e318e814e890167231e72be5c67fe3106
tonyedgal <tonyedgal@gmail.com>
2023-08-30 10:45:36 +0100

    feat: literal assignments and checking data types

--- data_types/data_types.py
+++ data_types/data_types.py
@@ -2,3 +2,16 @@
 # 1. all lowercase
 # 2. words separated by underscore
 # 3. no spaces
+
+# Data Types
+
+# String data type
+
+# literal assignment
+
+first = "Tony"
+last = "Edgal"
+
+print(type(first))
+print(type(last) == str)
+print(isinstance(last, str))
